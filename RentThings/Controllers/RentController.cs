4ddd65ad4ad822d632f57892a65cce828515459c
art264400 <art264400@yandex.ru>
2020-05-27 14:35:00 +0300

    New update

---
+++ RentThings/Controllers/RentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using RentThings.Interfaces;
+using RentThings.Services;
+
+namespace RentThings.Controllers
+{
+    public class RentController : Controller
+    {
+        public IRentService _rentService;
+
+        public RentController(IRentService rentService)
+        {
+            _rentService = rentService;
+        }
+        // GET: Rent
+        public ActionResult BrowseItems()
+        {
+            var products = _rentService.GetAllProducts();
+            return View(products);
+        }
+    }
+}
---
+++ RentThings/Interfaces/IRentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RentThings.Models;
+
+namespace RentThings.Interfaces
+{
+    public interface IRentService
+    {
+        Product[] GetAllProducts();
+    }
+}
